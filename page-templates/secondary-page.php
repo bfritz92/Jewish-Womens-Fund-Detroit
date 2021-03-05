8b3c01dd927f208a5f2fb7c8e4b657d55c7d9561
bfritz92 <67482120+bfritz92@users.noreply.github.com>
2021-03-05 11:17:12 -0500

    secondary page, might crash it all.

---
+++ page-templates/secondary-page.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Template Name: Full-width layout
+ * Template Post Type: post, page, event
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+get_header(); ?>
+
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+	<header class="entry-header alignwide">
+	</header>
+
+	<div class="entry-content">
+	<div class="wp-block-group alignfull page-splash has-black-color has-text-color has-background" style="background-color:#48348d">
+	<div class="wp-block-group__inner-container">
+	<figure class="wp-block-image alignfull size-large page-splash--img">
+	<?php twenty_twenty_one_post_thumbnail(); ?>
+	</figure>
+
+
+	<?php the_title( '<h1 class="entry-title alignwide page-splash--title has-white-color">', '</h1>' ); ?>
+
+</div></div>
+		<?php
+		the_content();
+
+		wp_link_pages(
+			array(
+				'before'   => '<nav class="page-links" aria-label="' . esc_attr__( 'Page', 'twentytwentyone' ) . '">',
+				'after'    => '</nav>',
+				/* translators: %: page number. */
+				'pagelink' => esc_html__( 'Page %', 'twentytwentyone' ),
+			)
+		);
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer alignwide">
+		<?php twenty_twenty_one_entry_meta_footer(); ?>
+	</footer><!-- .entry-footer -->
+
+	<?php if ( ! is_singular( 'attachment' ) ) : ?>
+		<?php get_template_part( 'template-parts/post/author-bio' ); ?>
+	<?php endif; ?>
+
+</article><!-- #post-<?php the_ID(); ?> -->
