6f10512c22b310d1671e76bcbc1076fbb7394b80
bfritz92 <67482120+bfritz92@users.noreply.github.com>
2021-03-05 11:40:12 -0500

    moving along....

--- page-templates/secondary-page.php
+++ page-templates/secondary-page.php
@@ -14,11 +14,11 @@ get_header(); ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	<header class="entry-header alignwide">
+	<header class="entry-header alignfull">
 		
 			<div class="wp-block-group alignfull page-splash has-black-color has-text-color has-background" style="background-color:#48348d">
 			<div class="wp-block-group__inner-container">
-		<?php twenty_twenty_one_post_thumbnail('wp-block-image alignfull size-large page-splash--img'); ?>
+		<?php twenty_twenty_one_post_thumbnail(); ?>
 		<?php the_title( '<h1 class="entry-title alignwide page-splash--title has-white-color">', '</h1>' ); ?>
 		</div></div>
 	</header>
