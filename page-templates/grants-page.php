e837caef4abf545279332c44726931742c38fe8a
bfritz92 <67482120+bfritz92@users.noreply.github.com>
2021-03-11 15:49:21 -0500

    remove extra styles, add search bar?

--- page-templates/grants-page.php
+++ page-templates/grants-page.php
@@ -15,23 +15,21 @@ get_header(); ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 	<header class="entry-header alignfull">
-		<div class="page-splash has-black-color has-text-color has-background" style="background-color:#48348d">
-		<?php the_title( '<h1 class="alignwide page-splash--title has-white-color pb1">', '</h1>' ); ?>
-        <?php
-$term = sanitize_text_field($_GET['term']);
-if(empty($term)){
-   $term = 'WordPress';
-}
-?>
+		<div class="has-black-color has-text-color has-background" style="background-color:#48348d">
+		<?php the_title( '<h1 class="alignwide has-white-color pb1">', '</h1>' ); ?>
+        <?php get_search_form() ?>
 		</div>
 	</header>
 
 	<div class="entry-content">
 
 		<?php
+        $term = sanitize_text_field($_GET['term']);
+        if(empty($term)){
+           $term = 'WordPress';
+        }
 		echo do_shortcode('[ajax_load_more post_type="post" posts_per_page="10" category="grants" category__not_in="7,6" search="'. $term .'"]');
         ?>
-		?>
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer alignwide">
