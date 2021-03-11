f0b5f7bbc75bbce756930021f08b2db5baf1152f
bfritz92 <67482120+bfritz92@users.noreply.github.com>
2021-03-11 14:39:27 -0500

    add grants page

---
+++ page-templates/grants-page.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Template Name: Grants Page
+ * Template Post Type: post, page, event
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+get_header(); ?>
+
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+	<header class="entry-header alignfull">
+		<div class="page-splash has-black-color has-text-color has-background" style="background-color:#48348d">
+		<?php the_title( '<h1 class="alignwide page-splash--title has-white-color pb1">', '</h1>' ); ?>
+        <?php
+$term = sanitize_text_field($_GET['term']);
+if(empty($term)){
+   $term = 'WordPress';
+}
+?>
+		</div>
+	</header>
+
+	<div class="entry-content">
+
+		<?php
+		echo do_shortcode('[ajax_load_more post_type="post" posts_per_page="10" category="grants" category__not_in="7,6" search="'. $term .'"]');
+        ?>
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer alignwide">
+		<?php twenty_twenty_one_entry_meta_footer(); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-<?php the_ID(); ?> -->
+
+<?php get_footer(); ?>
