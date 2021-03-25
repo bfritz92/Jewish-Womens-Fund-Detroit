452d87ba34634102649b70f7201a806ae7068f72
bfritz92 <67482120+bfritz92@users.noreply.github.com>
2021-03-25 13:42:49 -0400

    messing around more in the data-hooks

--- page-templates/grants-page.php
+++ page-templates/grants-page.php
@@ -24,8 +24,8 @@ get_header(); ?>
 		      			<button type="sumbit" class="submit">Search</button>
 	
 						<ul class="alm-filter-nav">
-						<li><a href="#" data-repeater="template_8" data-post-type="portfolio" data-posts-per-page="3" data-scroll="false" data-button-label="More Work">Recent Work</a></li>
-						<li><a href="#" data-repeater="default" data-post-type="post" data-post-category="grants" data-posts-per-page="5" data-scroll="true" data-button-label="More Articles">Recent Articles</a></li>
+						<li><a href="#" data-repeater="default" data-post-type="Grants" data-posts-per-page="3" data-scroll="false" data-button-label="More Work">Recent Work</a></li>
+						<li><a href="#" data-repeater="default" data-post-type="post" data-post-category="Grants" data-posts-per-page="5" data-scroll="true" data-button-label="More Articles">Recent Articles</a></li>
 						</ul>
 	      			</form>
 	   	</div>
