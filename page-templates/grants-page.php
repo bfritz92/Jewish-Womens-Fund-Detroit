a9e7e02b35d3876428ebdb74734514682c27ad28
bfritz92 <67482120+bfritz92@users.noreply.github.com>
2021-03-25 15:51:36 -0400

    add 2021 under years

--- page-templates/grants-page.php
+++ page-templates/grants-page.php
@@ -28,6 +28,7 @@ get_header(); ?>
 							<option value="2018">2018</option>
 							<option value="2019">2019</option>
 							<option value="2020">2020</option>
+							<option value="2021">2021</option>
 						</select> 
 		      			<button type="sumbit" class="submit">Search</button>
 	      			</form>
