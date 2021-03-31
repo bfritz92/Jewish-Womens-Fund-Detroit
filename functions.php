e629a6b6f444f905f3dcc1db2ef010c7e37dc67e
bfritz92 <67482120+bfritz92@users.noreply.github.com>
2021-03-31 13:45:13 -0400

    undo changes to functions.php

--- functions.php
+++ functions.php
@@ -665,17 +665,3 @@ add_action( 'wp_enqueue_scripts', function() {
 	$styles = wp_styles();
 	$styles->add_data( 'twenty-twenty-one-style', 'after', array() );
 }, 20 ); ?>
-
-<?php
-/*
-   functions.php
-   Use alm_query_args filter to pass data to SearchWP.
-   https://connekthq.com/plugins/ajax-load-more/docs/filter-hooks/#alm_query_args
-*/
-function my_alm_query_args_searchwp($args){   
-   $engine = 'default'; // default = default
-   $args = apply_filters('alm_searchwp', $args, $engine); // Make call to alm_searchwp filter
-   return $args;
-}
-add_filter( 'alm_query_args_searchwp', 'my_alm_query_args_searchwp'); ?>
-
