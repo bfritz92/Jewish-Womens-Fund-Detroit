0a43bb6bd924821d32e2b7412d65f545dbf92476
bfritz92 <67482120+bfritz92@users.noreply.github.com>
2021-04-13 14:38:17 -0400

    just a little more grant formatting

--- search-filter/results.php
+++ search-filter/results.php
@@ -58,8 +58,8 @@ if ( $query->have_posts() )
 			<div>
 			<h4><a href="<?php the_permalink(); ?>"><?php the_field ('name_of_organization'); ?></a></h4>
 			<p class="grant--title"><strong><?php the_title(); ?></strong></p>
-			<ul class="grant--info">
-				<li>
+			<ul class="grant--info mt1">
+				<li">
 					<strong>City/State:</strong>
 					<br />
 					<?php the_field ('city_state_country'); ?>
