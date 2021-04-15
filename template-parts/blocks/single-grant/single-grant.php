27d15a4eac68271c79d4ae68054585347984834b
bfritz92 <67482120+bfritz92@users.noreply.github.com>
2021-04-15 16:44:40 -0400

    herbs changes

--- template-parts/blocks/single-grant/single-grant.php
+++ template-parts/blocks/single-grant/single-grant.php
@@ -22,38 +22,53 @@ if( !empty($block['align']) ) {
 }
 // Load values and assing defaults.
 $fpip = get_field('block_single_grant');
+$standalone = get_field('standalone_page'); 
 ?>
 <!--Markup for Expand/Collapse-->
-<?php
-	// check if the flexible content field has rows of data
-	if( have_rows('block_single_grant') ):		
-		 // loop through the rows of data
-?>	
-	<section class="grant-container <?php echo $className ?>">		
+	<?php
+		// check if the flexible content field has rows of data
+		if( have_rows('block_single_grant') ):		
+			 // loop through the rows of data
+	?>
 		<?php
-			$b = 0;
 			while ( have_rows('block_single_grant') ) : the_row();
-			$image = get_sub_field('image');
-			$amount = get_sub_field('amount'); 
-			$partner = get_sub_field('partner'); 
-			$description = get_sub_field('description');
-			$id = get_sub_field('id');
-			$b++;
+			$post_object = get_sub_field('grant_name');
+			if( $post_object ):
+			$post = $post_object;
+            setup_postdata( $post );
+			$name_of_organization = get_field('name_of_organization', $post->ID);
+			$grant_title = get_field('grant_title', $post->ID);			
+			$city_state_country = get_field('city_state_country', $post->ID);
+			$grant_amount = get_field('grant_amount', $post->ID);
+			$grant_description = get_field('grant_description', $post->ID);
+			$year_grant_made = get_field('year_grant_made', $post->ID);
+			$organization_logo = get_field('organization_logo', $post->ID);
 		?>
-        <div id="<?php echo $id; ?>" class="grant single-grant">
+		<h4 class="has-purple-color grant--recip"><?php echo $name_of_organization; ?></h4>
+		<hr class="wp-block-separator">
+		<p class="grant--title"><strong><?php echo $grant_title; ?></strong></p>
+		<ul class="grant--info">
+			<?php if ($city_state_country) : ?>
+				<li><strong>City/State:</strong><br><?php echo $city_state_country; ?></li>
+			<?php endif; ?>
+			<?php if ($grant_amount) : ?>
+				<li><strong>Amount:</strong><br><?php echo $grant_amount; ?></li>
+			<?php endif; ?>
+			<?php if ($year_grant_made) : ?>
+				<li><strong>Year:</strong><br><?php echo $year_grant_made; ?></li>
+			<?php endif; ?>
+		</ul>
+		<?php if ($grant_description) : ?>
+			<p class="grant--desc"><?php echo $grant_description; ?></p>
+		<?php endif; ?>
+		<?php if ($organization_logo) : ?>
+			<figure class="wp-block-image size-large grant--logo">
+				<img loading="lazy" width="300" height="62" src="<?php echo $organization_logo['url']; ?>" alt="<?php echo $organization_logo['alt']; ?>" class="wp-image-1762">
+			</figure>
+		<?php endif; ?>
+		<?php wp_reset_query(); // IMPORTANT - reset the $post object so the rest of the page works correctly ?>
+		<?php endif; ?>
+   
+				<?php endwhile;	?>
 
-            <!-- <img class="grant--image" src="<?php echo $image?>"> -->
-            <?php echo wp_get_attachment_image( $image, 'large' );?>
-            <p class="grant--amount"><?php the_sub_field('amount'); ?></p>
-            <div class="grant--info">
-                <h3 class="grant--partner"><?php the_sub_field('partner');?></h3>
-                <p><?php the_sub_field('description');?></p>	
-            </div>
-		</div>
-	
-<?php endwhile; ?>
-           
-            </section>
-<?php else : ?>
-<!--	// no layouts found -->
-<?php endif; ?>				
+	<?php endif; ?>	
